13370e7fc9ca60095b7f99b06a7ffe6f58d96a06
Bskasan <b.kasan@yandex.com>
2023-01-21 11:41:11 +0200

    setItem local storage and create todo beginning

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const todoInput = document.getElementById("input-value");
 const addBtn = document.querySelector("#add-btn");
 const todoUl = document.querySelector("#list");
 
+//? global array for todo obj
 let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
 //? Load event vs. DomContentLoaded (Interview Question)
@@ -27,9 +28,24 @@ addBtn.addEventListener("click", (e) => {
   }
   //* alert("continue");
   const newTodo = {
-    id : new Date().getTime , //* Unique id with ms of now
-    completed : false, //* Status
-    text : todoInput.value, //* User Input
+    id: new Date().getTime, //* Unique id with ms of now
+    completed: false, //* Status
+    text: todoInput.value, //* User Input
+  };
 
-  }
+  createTodo(newTodo);
+
+  todoList.push(newTodo);
+  //? localStorage todoList Update, set item given above by using getItem("todoList")
+  //? Local Storage vs. Session Storage (Interview Question)
+  //!!!!!!!! Stringify !!!!!!!!!
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+
+  //? event.target vs. event.currentTarget (Interview Question)
+  e.target.closest("form").reset();
 });
+
+const createTodo = (newTodo) => {
+  //* todo item creation
+  
+};
