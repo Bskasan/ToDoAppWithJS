6f559df428abf71c8396868ad11160cb1c034ea8
Bskasan <b.kasan@yandex.com>
2023-01-21 10:35:51 +0200

    js started elements

---
+++ app.js
@@ -0,0 +1,7 @@
+//* Elements - DOM Catching
+//? Fastest one, if there is a 'id' you should use getElementById.
+const todoInput = document.getElementById("input-value");
+const addBtn = document.querySelector("#add-btn");
+const todoUl = document.querySelector("#list");
+
+let todoList = 
