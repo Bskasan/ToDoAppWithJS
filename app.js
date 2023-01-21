6d4595a23183a050b5470be78383366e17f4c908
Bskasan <b.kasan@yandex.com>
2023-01-21 10:45:34 +0200

    getTodoListFromLocalStorage method

--- app.js
+++ app.js
@@ -4,4 +4,21 @@ const todoInput = document.getElementById("input-value");
 const addBtn = document.querySelector("#add-btn");
 const todoUl = document.querySelector("#list");
 
-let todoList = 
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+
+//? Load event vs. DomContentLoaded (Interview Question)
+window.addEventListener("load", () => {
+  getTodoListFromLocalStorage();
+});
+
+const getTodoListFromLocalStorage = () => {
+  //* get TodoList from localStorage and load to UI
+};
+
+//* form => submit event
+//! Alternative Way
+//! form.addEventListener("submit", ()=>{});
+
+addBtn.addEventListener("click", () => {
+    
+});
